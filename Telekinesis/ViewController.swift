b0d5eaaf042c58352586adfdbd9dc7e89deaec3e
Michael Camilleri <mike@inqk.net>
2020-07-28 16:55:36 +0900

    Add example JavaScript processor
    Telekinesis is a text manipulation app that runs text inputted by the
    user through a 'processor'. A processor is a JavaScript function that is
    executed in a JSContext. The JavaScript function is specified in a
    JavaScript file that makes a function, process(text), available in the
    global namespace.
    
    This commit adds an example of a JavaScript processor to the directory
    Processors/. The main View Controller loads the processor and uses it to
    process text. The loading does minimal error checking and little
    feedback is provided to the user if the JavaScript file has errors in
    it.

--- Telekinesis/ViewController.swift
+++ Telekinesis/ViewController.swift
@@ -7,20 +7,41 @@
 //
 
 import UIKit
+import JavaScriptCore
 
 class ViewController: UIViewController, UITextViewDelegate {
     @IBOutlet var textPreview: UITextView!
     @IBOutlet var textEditor: UITextView!
     
+    var jsProcessFunction: JSValue? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         textEditor.delegate = self
+        setupProcessor()
+    }
+    
+    func setupProcessor(using filename: String = "wrap-at-72-chars") {
+        guard let jsContext = JSContext() else { return }
+
+        if let jsSourcePath = Bundle.main.path(forResource: filename, ofType: "js") {
+            do {
+                let jsSourceContents = try String(contentsOfFile: jsSourcePath)
+                jsContext.evaluateScript(jsSourceContents)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        jsProcessFunction = jsContext.objectForKeyedSubscript("process")
     }
     
     // UITextViewDelegate Methods
     
     func textViewDidChange(_ textView: UITextView) {
-        textPreview.text = textView.text
+        guard let result = jsProcessFunction?.call(withArguments: [textView.text ?? ""]) else { return }
+        
+        textPreview.text = result.toString()
     }
 }
