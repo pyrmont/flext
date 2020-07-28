45e053b348f87af98596d9894f129e4d1a7d5076
Michael Camilleri <mike@inqk.net>
2020-07-28 10:03:34 +0900

    Initial Commit

---
+++ Telekinesis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telekinesis
+//
+//  Created by Michael Camilleri on 28/7/20.
+//  Copyright © 2020 Michael Camilleri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
