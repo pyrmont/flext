20a17078e2cd71590ea27926a6065a7f7da5daae
Michael Camilleri <mike@inqk.net>
2020-08-22 12:46:13 +0900

    Add heuristic to select an element when using extension with webpages
    When using Telekinesis with a webpage, it will first attempt to get
    the element with focus. However, on an iPhone, a user will have to blur
    the text area being edited in order to open the share menu.
    
    This commit adds a simple heuristic to find the 'intended' text area.
    The heuristic simply looks for the text area with the longest value. If
    no text area has a value, the heuristic selects the first text area.

--- ActionExtension/Action.js
+++ ActionExtension/Action.js
@@ -3,16 +3,59 @@ var Action = function() {};
 Action.prototype = {
 
 run: function(parameters) {
-    var text = document.activeElement.value;
+    let text = document.activeElement.value;
+    
+    if (!text) {
+        let longest = null;
+        const textareas = document.getElementsByTagName('textarea');
+        
+        for (const textarea of textareas) {
+            if (longest == null) {
+                longest = textarea;
+                continue;
+            }
+            
+            if (textarea.value) {
+                if (!longest.value) {
+                    longest = textarea;
+                } else if (longest.value.length < textarea.value.length) {
+                    longest = textarea;
+                }
+            }
+        }
+        
+        text = longest.value;
+    }
+    
     parameters.completionFunction({"text": text});
 },
 
 finalize: function(parameters) {
     if (parameters["text"]) {
-        var el = document.activeElement
-        if (el) {
-            el.value = parameters["text"]
+        let el = document.activeElement;
+        if (!el.value) {
+            let longest = null;
+            const textareas = document.getElementsByTagName('textarea');
+            
+            for (const textarea of textareas) {
+                if (longest == null) {
+                    longest = textarea;
+                    continue;
+                }
+                
+                if (textarea.value) {
+                    if (!longest.value) {
+                        longest = textarea;
+                    } else if (longest.value.length < textarea.value.length) {
+                        longest = textarea;
+                    }
+                }
+            }
+            
+            el = longest;
         }
+        
+        el.value = parameters["text"];
     }
 }
 
