1b5fe46912330f914ce61204dd27d2489f14bc19
Michael Camilleri <mike@inqk.net>
2020-09-09 14:01:21 +0900

    Make Mac Catalyst version part of Flext target
    The Mac Catalyst version was developed as a separate target but that
    doesn't seem like the best long-term approach. This commit moves the
    files used by the Mac Catalyst version into the main Flext target. It
    loads a separate storyboard for the main interface by defining the
    storyboard to use a variable name `${MAIN_STORYBOARD}` and then sets
    that conditionally depending on the SDK (either `Main` or `MainMac`).

---
+++ Flext/ViewControllers/SplitViewController.swift
@@ -0,0 +1,86 @@
+//
+//  SplitViewController.swift
+//  FlextMac
+//
+//  Created by Michael Camilleri on 1/9/20.
+//  Copyright © 2020 Michael Camilleri. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+
+    var processorListController: SplitProcessorListViewController!
+    var editorController: SplitEditorViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        #if targetEnvironment(macCatalyst)
+        self.primaryBackgroundStyle = .sidebar
+        #endif
+
+        for viewController in viewControllers {
+            if let editorController = viewController as? SplitEditorViewController {
+                self.editorController = editorController
+            } else if let processorListController = (viewController as? UINavigationController)?.topViewController as? SplitProcessorListViewController{
+                self.processorListController = processorListController
+            }
+        }
+
+        processorListController.editor = editorController
+    }
+
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        switch action {
+        case #selector(selectNextProcessorMenuAction):
+            return processorListController.hasNextProcessor
+        case #selector(selectPreviousProcessorMenuAction):
+            return processorListController.hasPreviousProcessor
+        case #selector(resetInputMenuAction):
+            guard let text = editorController.enteredText.value else { return false }
+            return !text.isEmpty
+        case #selector(copyOutputMenuAction):
+            guard let text = editorController.enteredText.value else { return false }
+            return !text.isEmpty
+        default:
+            break
+        }
+
+        return super.canPerformAction(action, withSender: sender)
+    }
+
+    override func validate(_ command: UICommand) {
+        switch command.title {
+        case "Favourite":
+            guard let processor = processorListController.processor(at: processorListController.settings.selectedProcessorPath) else { break }
+            if processor.isFavourited {
+                command.title = "Unfavourite"
+            }
+        default:
+            break
+        }
+
+        super.validate(command)
+    }
+
+    @IBAction func selectNextProcessorMenuAction() {
+        processorListController.selectNextProcessor()
+    }
+
+    @IBAction func selectPreviousProcessorMenuAction() {
+        processorListController.selectPreviousProcessor()
+    }
+
+    @IBAction func toggleFavouriteStatusMenuAction() {
+        processorListController.toggleFavouriteStatus()
+    }
+
+    @IBAction func resetInputMenuAction() {
+        editorController.resetInput()
+    }
+
+    @IBAction func copyOutputMenuAction() {
+        editorController.copyOutput()
+    }
+}
